ea39b71a53264155b5fab1ef9f40454927871a1d
Paula Lavalle <lavalle.paula@gmail.com>
2020-08-17 14:24:36 -0700

    trying to debug this experimental syntax jsx plugin error

--- demo/src/screens/home/index.js
+++ demo/src/screens/home/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 import Header from "../../../../src/components/Header";
 import Features from "../../../../src/components/Features";
-// import Preview from "../../../../src/components/Preview";
+import Preview from "../../../../src/components/Preview";
 import CustomSection from "../../../../src/components/CustomSection";
 import GetStarted from "../../../../src/components/GetStarted";
 import FeaturedOSS from "../../../../src/components/FeaturedOSS";
@@ -16,12 +16,13 @@ const Wrapper = styled.div`
 `;
 
 function App() {
-  // const hasPreview = !!config.preview;
+  const hasPreview = !!config.preview;
   const hasCustomSection = !!config.customSection;
   return (
     <Wrapper>
       <Header {...config.header} linkComponent={config.linkComponent} />
       <Features {...config.features} />
+      {hasPreview ? <Preview {...config.preview} /> : null}
       {hasCustomSection ? <CustomSection {...config.customSection} /> : null}
       <GetStarted {...config.getStarted} linkComponent={config.linkComponent} />
       <FeaturedOSS {...config.featuredOss} />
