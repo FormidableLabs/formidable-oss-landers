3ecc17e2760fb87b30bb2d5ed87084c22690bcfe
Erika Smith <erikaannesmith@gmail.com>
2020-12-08 11:57:55 -0500

    Import footer proptypes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import PropTypes from "prop-types";
 import Grid from "./Grid";
 import Section from "./Section";
 import Text from "./Text";
