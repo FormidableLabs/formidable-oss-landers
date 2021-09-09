aa260ae9d3cf3efb6ffff0989f554f214c9fc8e3
Erika Smith <erika.smith@formidable.com>
2021-09-09 13:04:36 -0400

    Cleanup

--- docs/src/styles/index.js
+++ docs/src/styles/index.js
@@ -2,36 +2,20 @@
 import { css } from "styled-components";
 
 /*
-Set default responsive box-shadow using theme colors
-Usage: ${boxShadow("darkNeutral")};
-*/
-export function boxShadow(color) {
-  return css`
-    box-shadow: ${(props) => props.theme.boxShadows.small(color)};
-    ${(props) => props.theme.media.desktop`
-      box-shadow: ${(props) => props.theme.boxShadows.large(color)}; 
-    `};
-  `;
-}
-
-/*
 Set color by theme
 Usage: ${color("darkNeutral")};
 */
-export function color(name) {
-  return css`
-    ${(props) => {
-      if (name) {
-        if (props.theme.colors[name]) {
-          return `color: ${props.theme.colors[name]};`;
-        } else {
-          /* eslint-disable-next-line no-console */
-          console.warn(
-            `Could not find '${name}' in available list of theme colors: `,
-            Object.keys(props.theme.colors)
-          );
-        }
+export const color = (name) => css`
+  ${(props) => {
+    if (name) {
+      if (props.theme.colors[name]) {
+        return `color: ${props.theme.colors[name]};`;
+      } else {
+        console.warn(
+          `Could not find '${name}' in available list of theme colors: `,
+          Object.keys(props.theme.colors)
+        );
       }
-    }}
-  `;
-}
+    }
+  }}
+`;
