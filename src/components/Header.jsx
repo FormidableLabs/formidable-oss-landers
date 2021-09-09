f06b86e4206dc92c7b2c43ce3d07dab2a7d5701c
Erika Smith <erika.smith@formidable.com>
2021-09-09 13:40:28 -0400

    Readd PropTypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { FeaturedBadge } from "formidable-oss-badges";
 import Grid from "./Grid";
 import HeaderInstall from "./HeaderInstall";
